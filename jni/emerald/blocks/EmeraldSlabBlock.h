4a60a1eb5b7429e7dd25023a3e6cfdec796924a3
TrinityDevelopers <trinitydevelopers4christ@gmail.com>
2016-05-06 16:48:38 -0500

    Update EmeraldSlabBlock.h

--- jni/emerald/blocks/EmeraldSlabBlock.h
+++ jni/emerald/blocks/EmeraldSlabBlock.h
@@ -5,6 +5,9 @@
 class EmeraldSlabBlock : public SlabBlock
 {
 public:
+	TextureUVCoordinateSet emeraldTex;
+
 	EmeraldSlabBlock(int blockId);
 	virtual bool use(Player&, const BlockPos&);
+	virtual const TextureUVCoordinateSet& getTexture(signed char);
 };
